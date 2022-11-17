7ed8c51b222b9e6ae8d6ff7615cd76aefe977a22
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-11-17 10:42:16 +0700

    Update __main__.py

--- ProjectMan/__main__.py
+++ ProjectMan/__main__.py
@@ -11,15 +11,17 @@ from pyrogram import idle
 from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
-from ProjectMan import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
-from ProjectMan.helpers.misc import create_botlog, git, heroku
+from PunyaAlby import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from PunyaAlby.helpers.misc import create_botlog, git, heroku
 
 MSG_ON = """
-🔥 **PyroMan-Userbot Berhasil Di Aktifkan**
-━━
+**ALBY-PYROBOT DIAKTIFKAN**📍
+      (\︵/) 
+　⫺( •ᆺ•)⫹ 
+┏━∪ ━━━━━━━
 ➠ **Userbot Version -** `{}`
-➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
-━━
+➠ **Ketik** `{}alby` **untuk Mengecheck Bot**
+┗━━━━━━━━━
 """
 
 
@@ -28,28 +30,38 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("Lunatic0de")
-            await bot.join_chat("SharingUserbot")
+            await bot.join_chat("ruangdiskusikami")
+            await bot.join_chat("ruangprojects")
             try:
                 await bot.send_message(
                     BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
                 )
             except BaseException:
                 pass
-            LOGGER("ProjectMan").info(
+            LOGGER("PunyaAlby").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("ProjectMan").info(f"PyroMan-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("PunyaAlby").info(f"ALBY-PYROBOT v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
     if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
+    if bot1 and str(BOTLOG_CHATID).startswith("-100"):
+        bot1.me = await bot1.get_me()
+        chat = await bot1.get_chat(BOTLOG_CHATID)
+        desc = "Group Log untuk ALBY-PYROBOT.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @ruangprojects ✨"
+        lolo = f"LOGS FOR {bot1.me.first_name}"
+        if chat.description != desc:
+            await bot1.set_chat_description(BOTLOG_CHATID, desc)
+        if chat.title != lolo:
+            await bot1.set_chat_title(BOTLOG_CHATID, lolo)
+        await bot1.set_chat_photo(BOTLOG_CHATID, photo="PunyaAlby/helpers/homepage.png")
     await idle()
     await aiosession.close()
 
 
 if __name__ == "__main__":
-    LOGGER("ProjectMan").info("Starting PyroMan-UserBot")
+    LOGGER("PunyaAlby").info("Starting ALBY-PYROBOT")
     install()
     git()
     heroku()
