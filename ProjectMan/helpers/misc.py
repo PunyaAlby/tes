add3485d78ab40795ac6a0243c8d74d9498fcf09
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-11-17 10:43:32 +0700

    Update misc.py

--- ProjectMan/helpers/misc.py
+++ ProjectMan/helpers/misc.py
@@ -6,7 +6,6 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-
 import asyncio
 import shlex
 import socket
@@ -18,7 +17,7 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
 from config import BRANCH, GIT_TOKEN, HEROKU_API_KEY, HEROKU_APP_NAME, REPO_URL
-from ProjectMan import LOGGER
+from PunyaAlby import LOGGER
 
 HAPP = None
 
@@ -69,9 +68,9 @@ def git():
         UPSTREAM_REPO = REPO_URL
     try:
         repo = Repo()
-        LOGGER("ProjectMan").info(f"Git Client Found")
+        LOGGER("PunyaAlby").info(f"Git Client Found")
     except GitCommandError:
-        LOGGER("ProjectMan").info(f"Invalid Git Command")
+        LOGGER("PunyaAlby").info(f"Invalid Git Command")
     except InvalidGitRepositoryError:
         repo = Repo.init()
         if "origin" in repo.remotes:
@@ -96,7 +95,7 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -U -r requirements.txt")
-        LOGGER("ProjectMan").info("Fetched Latest Updates")
+        LOGGER("PunyaAlby").info("Fetched Latest Updates")
 
 
 def is_heroku():
@@ -110,7 +109,7 @@ def heroku():
             try:
                 Heroku = heroku3.from_key(HEROKU_API_KEY)
                 HAPP = Heroku.app(HEROKU_APP_NAME)
-                LOGGER("ProjectMan").info(f"Heroku App Configured")
+                LOGGER("PunyaAlby").info(f"Heroku App Configured")
             except BaseException as e:
                 LOGGER("Heroku").error(e)
                 LOGGER("Heroku").info(
@@ -125,10 +124,10 @@ async def in_heroku():
 async def create_botlog(client):
     if HAPP is None:
         return
-    LOGGER("ProjectMan").info(
+    LOGGER("PunyaAlby").info(
         "TUNGGU SEBENTAR. SEDANG MEMBUAT GROUP LOG USERBOT UNTUK ANDA"
     )
-    desc = "Group Log untuk PyroMan-UserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @Lunatic0de ✨"
+    desc = "Group Log untuk ALBY-PYROBOT.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @ruangdiskusikami ✨"
     try:
         gruplog = await client.create_supergroup("Log UserBot", desc)
         if await in_heroku():
@@ -138,6 +137,6 @@ async def create_botlog(client):
             path = dotenv.find_dotenv("config.env")
             dotenv.set_key(path, "BOTLOG_CHATID", gruplog.id)
     except Exception:
-        LOGGER("ProjectMan").warning(
+        LOGGER("PunyaAlby").warning(
             "var BOTLOG_CHATID kamu belum di isi. Buatlah grup telegram dan masukan bot @MissRose_bot lalu ketik /id Masukan id grup nya di var BOTLOG_CHATID"
         )
