800157d51d27a8798da14849d15af5a04165da94
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-11-18 08:45:28 +0700

    Update __main__.py

--- PunyaAlby/__main__.py
+++ PunyaAlby/__main__.py
@@ -7,10 +7,23 @@
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
 
-import importlib
 
-from pyrogram import idle
 from uvloop import install
+import time
+import logging
+import importlib
+import random
+import sys
+import traceback
+import threading
+import asyncio
+
+import pyrogram
+from pyrogram import filters, idle
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from PunyaAlby import app, log, Command, isCi
+from PunyaAlby.modules import ALL_PLUGINS
 from config import BOT_VER, CMD_HANDLER
 from PunyaAlby import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
 from PunyaAlby.helpers.misc import create_botlog, git, heroku
@@ -60,10 +73,43 @@ async def main():
     await idle()
     await aiosession.close()
 
+BOT_RUNTIME = 0
+HELP_COMMANDS = {}
+
+
+loop = asyncio.get_event_loop()
+
+async def get_runtime():
+	return BOT_RUNTIME
+
+async def start_bot():
+	await app.start()
+	for module in ALL_PLUGINS:
+		imported_module = importlib.import_module("PunyaAlby.modules." + module)
+		if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+			imported_module.__MODULE__ = imported_module.__MODULE__
+		if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+			if not imported_module.__MODULE__.lower() in HELP_COMMANDS:
+				HELP_COMMANDS[imported_module.__MODULE__.lower()] = imported_module
+			else:
+				raise Exception("Can't have two modules with the same name! Please change one")
+		if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+			HELP_COMMANDS[imported_module.__MODULE__.lower()] = imported_module
+	log.info("-----------------------")
+	log.info("Plugins loaded: " + str(ALL_PLUGINS))
+	log.info("-----------------------")
+	log.info("Bot run successfully!")
+
+	if isCi:
+		log.info("Test is passed!")
+	else:
+		await idle()
 
 if __name__ == "__main__":
     LOGGER("PunyaAlby").info("Starting ALBY-PYROBOT")
     install()
     git()
     heroku()
+    BOT_RUNTIME = int(time.time())
     LOOP.run_until_complete(main())
+    loop.run_until_complete(start_bot())
