c51851a39800b71782ba9bd3cfe7cc3a966e6014
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-11-19 21:20:28 +0700

    Update __init__.py

--- PunyaAlby/__init__.py
+++ PunyaAlby/__init__.py
@@ -8,17 +8,6 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 import asyncio
-import os
-import time
-from os import listdir, mkdir
-
-import heroku3
-from aiohttp import ClientSession
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
-from rich.console import Console
-from rich.table import Table
-import asyncio
 import logging
 import sys
 import time
@@ -111,141 +100,69 @@ TEMP_SETTINGS["PM_COUNT"] = {}
 TEMP_SETTINGS["PM_LAST_MSG"] = {}
 
 
-### testing
-from PunyaAlby.config import BOTLOG_CHATID, STRING_SESSION1, UPSTREAM_BRANCH, UPSTREAM_REPO
-from PunyaAlby.clientbot import client, robot
-from PunyaAlby.utils.times import time_to_seconds
-from PunyaAlby.utils.tasks import install_requirements
-
-
-loop = asyncio.get_event_loop()
-console = Console()
-
-
-### Heroku Shit
-UPSTREAM_BRANCH = UPSTREAM_BRANCH
-UPSTREAM_REPO = UPSTREAM_REPO
-
-### Modules
-MOD_LOAD = []
-MOD_NOLOAD = []
-
-### Boot Time
-boottime = time.time()
-
-### Clients
-aiohttpsession = ClientSession()
-robot = robot
-
-### Config
-BOTLOG_CHATID = BOTLOG_CHATID
-
-### Bot Info
-BOT_ID = 0
-BOT_NAME = ""
-BOT_USERNAME = ""
-
-### Assistant Info
-ASSIDS = []
-ASSID = 0
-ASSNAME = ""
-ASSUSERNAME = ""
-ASSMENTION = ""
-random_assistant = []
-
-
-async def initiate_bot():
-    global ASSIDS
-    global BOT_ID, BOT_NAME, BOT_USERNAME
-    global ASSID, ASSNAME, ASSMENTION, ASSUSERNAME
-    global Heroku_cli, Heroku_app
-    os.system("clear")
-    header = Table(show_header=True, header_style="bold yellow")
-    header.add_column(
-        "ALBY PYROBOT : Best Ever Userbot"
+bot1 = (
+    Client(
+        name="bot1",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION1,
+        plugins=dict(root="PunyaAlby/modules"),
     )
-    console.print(header)
-    with console.status(
-        "[magenta] ALBY PYROBOT Booting...",
-    ) as status:
-        console.print("┌ [red]Booting Up The Bot Clients...\n")
-        await robot.start()
-        console.print("└ [green]Booted Bot Client")
-        console.print("\n┌ [red]Booting Up The User Client...")
-        if STRING_SESSION1 != "None":
-            random_assistant.append(1)
-            console.print("├ [yellow]Booted User Client")
-        
-        console.print("└ [green]All Client Bots Booted Successfully!")
-        if "raw_files" not in listdir():
-            mkdir("raw_files")
-        if "downloads" not in listdir():
-            mkdir("downloads")
-        if "cache" not in listdir():
-            mkdir("cache")
-        if "search" not in listdir():
-            mkdir("search")
-        console.print("\n┌ [red]Loading Clients Information...")
-        getme = await robot.get_me()
-        BOT_ID = getme.id
-        if getme.last_name:
-            BOT_NAME = getme.first_name + " " + getme.last_name
-        else:
-            BOT_NAME = getme.first_name
-        BOT_USERNAME = getme.username
-        if STRING_SESSION1 != "None":
-            getme = await client.get_me()
-            ASSID = getme.id
-            ASSIDS.append(ASSID)
-            ASSNAME = (
-                f"{getme.first_name} {getme.last_name}"
-                if getme.last_name
-                else getme.first_name
-            )
-            ASSUSERNAME = getme.username
-            ASSMENTION = getme.mention
-        console.print("└ [green]Loaded Clients Information!")
-        try:
-            repo = Repo()
-        except GitCommandError:
-            console.print("┌ [red] Checking Git Updates!")
-            console.print("└ [red]Git Command Error\n")
-            return
-        except InvalidGitRepositoryError:
-            console.print("┌ [red] Checking Git Updates!")
-            repo = Repo.init()
-            if "origin" in repo.remotes:
-                origin = repo.remote("origin")
-            else:
-                origin = repo.create_remote("origin", UPSTREAM_REPO)
-            origin.fetch()
-            repo.create_head(UPSTREAM_BRANCH, origin.refs[UPSTREAM_BRANCH])
-            repo.heads[UPSTREAM_BRANCH].set_tracking_branch(
-                origin.refs[UPSTREAM_BRANCH]
-            )
-            repo.heads[UPSTREAM_BRANCH].checkout(True)
-            try:
-                repo.create_remote("origin", UPSTREAM_REPO)
-            except BaseException:
-                pass
-            nrs = repo.remote("origin")
-            nrs.fetch(UPSTREAM_BRANCH)
-            try:
-                nrs.pull(UPSTREAM_BRANCH)
-            except GitCommandError:
-                repo.git.reset("--hard", "FETCH_HEAD")
-            await install_requirements(
-                "pip3 install --no-cache-dir -r Installer"
-            )
-            console.print("└ [red]Git Client Update Completed\n")
+    if STRING_SESSION1
+    else None
+)
+
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION2,
+        plugins=dict(root="PunyaAlby/modules"),
+    )
+    if STRING_SESSION2
+    else None
+)
+
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION3,
+        plugins=dict(root="PunyaAlby/modules"),
+    )
+    if STRING_SESSION3
+    else None
+)
+
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION4,
+        plugins=dict(root="PunyaAlby/modules"),
+    )
+    if STRING_SESSION4
+    else None
+)
+
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION5,
+        plugins=dict(root="PunyaAlby/modules"),
+    )
+    if STRING_SESSION5
+    else None
+)
 
 
-loop.run_until_complete(initiate_bot())
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5] if bot]
 
-
-def init_db():
-    global db_mem
-    db_mem = {}
-
-
-init_db()
+for bot in bots:
+    if not hasattr(bot, "group_call"):
+        setattr(bot, "group_call", GroupCallFactory(bot).get_group_call())
